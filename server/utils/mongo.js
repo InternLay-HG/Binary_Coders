67cdc330c5b5eae06f7c3790511f91b9a9584527
Sbrjt <champa.281982@gmail.com>
2024-11-03 20:58:25 +0530

    integrated from rohit

--- server/utils/mongo.js
+++ server/utils/mongo.js
@@ -1,8 +1,23 @@
 import 'dotenv/config'
 import mongoose from 'mongoose'
 
-await mongoose.connect(process.env.MONGO_URI)
+await mongoose.connect(process.env.MONGO_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+})
 const db = mongoose.connection.db
-const users = db.collection('users')
 
-export { users, db as default }
+const users = mongoose.model(
+	'users',
+	new mongoose.Schema({
+		_id: { type: String, required: true },
+		name: String,
+		email: String,
+		picture: String,
+		isAthlete: { type: String, default: 'false' },
+		isCoach: { type: String, default: 'false' },
+		isDirector: { type: String, default: 'false' },
+	})
+)
+
+export { db as default, users }
