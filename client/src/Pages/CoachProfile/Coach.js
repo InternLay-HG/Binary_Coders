e067d74b5240f40421e0f134b29ca43041683eb4
Abhrajit Chatterjee <155379279+Abhrajit-2005@users.noreply.github.com>
2024-11-10 19:20:48 +0530

    Add files via upload

---
+++ client/src/Pages/CoachProfile/Coach.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Sidebar from '../Components/sidenav';
+import Navbar from '../Components/Navbar';
+import Profile from './Profile';  
+import Events from './Events';     
+import Members from './Members';     
+import Budget from './Budget';     
+import Updates from './Updates';     
+import ContextWrapper from '../context/ContextWrapper';
+
+const Coach_Profile = () => {
+    const [isSidebarActive, setIsSidebarActive] = useState(false);
+
+    const toggleSidebar = () => {
+        setIsSidebarActive(!isSidebarActive);
+    };
+
+    return (
+        <Router>
+            <div className="flex w-full h-screen">
+                {/* Sidebar */}
+                <Sidebar isActive={isSidebarActive} toggleSidebar={toggleSidebar} />
+
+                <div 
+                    className="flex-1 h-full transition-all duration-500" 
+                    style={{ marginLeft: isSidebarActive ? '138px' : '60px', marginTop: '36px' }}
+                >
+                    {/* Navbar */}
+                    <Navbar />
+
+                    {/* Main Content */}
+                    <main className="p-0 pt-5">
+                        <Routes>
+                            <Route path="/profile" element={<Profile sidebarWidth={isSidebarActive ? "10px" : "10px"} />} />
+                            <Route path="/members/*" element={<Members sidebarWidth={isSidebarActive ? "10px" : "10px"}/>} />
+                            <Route path="/events" element={<ContextWrapper><Events sidebarWidth={isSidebarActive ? "42px" : "20px"}/></ContextWrapper>} />
+                            <Route path="/budget" element={<Budget sidebarWidth={isSidebarActive ? "10px" : "10px"}/>} />
+                            <Route path="/update" element={<Updates sidebarWidth={isSidebarActive ? "42px" : "20px"}/>} />
+                            <Route path="/" element={<Navigate to="/profile" />} /> {/* Default route */}
+                        </Routes>
+                    </main>
+                </div>
+            </div>
+        </Router>
+    );
+};
+
+export default Coach_Profile;
